649a6d04df98b805c87b7bf4f5f7e5de7e71c992
kailin <kailin@torvadata.com>
2022-02-24 20:44:25 -0800

    added timeout logic and push

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //Listen for Submit
 document.getElementById("loan-form").addEventListener('submit', calculateResults);
 
-
 function calculateResults(e){
 
   const amount = document.getElementById('amount');
@@ -18,14 +17,44 @@ const calculatedPayments = parseFloat(years.value)*12;
 
 //Compute Monthly Payment:
 const x = Math.pow(1 + calculatedInterest, calculatedPayments);
-const monthly = (principal*x*calculatedInterest)/(x-1);
+const monthly = (principal * x * calculatedInterest)/(x-1);
 
 if(isFinite(monthly)){
   monthlyPayment.value = monthly.toFixed(2);
   totalPayment.value = (monthly*calculatedPayments.toFixed(2));
   totalInterest.value = ((monthly * calculatedPayments)- principal).toFixed(2);
 } else {
-console.log("Please check your numbers")
+  showError("Please Check Your Numbers");
+
 }
   e.preventDefault();
+}
+
+
+function showError(error) {
+//Create Div
+  const errorDiv = document.createElement('div');
+
+  //Get Parent Element
+  const card = document.querySelector(".card-body");
+
+  //Get Heading Element
+  const heading = document.querySelector(".heading");
+
+  //Add bootstrap class
+   errorDiv.className = "alert alert-danger";
+
+   //Create Text Node & Append to Div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  //Insert errorDiv above Heading:
+  card.insertBefore(errorDiv, heading)
+
+  //Clear Error after 3 seconds 
+  setTimeout(clearError, 3000);
+  }
+
+  //Clear Error Function
+function clearError(){
+  document.querySelector('.alert').remove();
 }
